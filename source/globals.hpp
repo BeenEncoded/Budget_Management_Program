e91864145a8fe433c679b8062250ccdc60cdcca6
Jonathan <jonathancw@me.com>
2015-02-19 13:11:44 -0800

    initial commit;
    Signed-off-by: Jonathan <jonathancw@me.com>

---
+++ source/globals.hpp
@@ -0,0 +1,6 @@
+#ifndef GLOBALS_HPP_INCLUDED
+#define GLOBALS_HPP_INCLUDED
+
+#include "common/global/global_defines.hpp"
+
+#endif
---
+++ source/main.cpp
@@ -0,0 +1,6 @@
+
+
+int main(int count __attribute__((unused)), char **vec __attribute__((unused)))
+{
+    return 0;
+}
